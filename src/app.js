543c38693c77dd62081466fecb52d93f0d55d986
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-11-16 12:43:16 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,17 +19,17 @@ const globalLimiter = rateLimit({
 
 app.use(cors())
 app.use(helmet())
-app.use('*', globalLimiter)
-app.all('/ping', coreUtils.ping)
+app.use(globalLimiter)
 app.use(express.json({ limit: '8kb' }))
+app.get('/ping', coreUtils.ping)
 app.use(mongoSanitize())
 app.use(xss())
 
 app.response.success = coreUtils.success
 app.request.getBody = coreUtils.getBody
 
-app.use('/v1', router)
-app.use('*', coreUtils.notFound)
+app.use(router)
+app.use(coreUtils.notFound)
 app.use(coreUtils.errorHandler)
 
 module.exports = app
